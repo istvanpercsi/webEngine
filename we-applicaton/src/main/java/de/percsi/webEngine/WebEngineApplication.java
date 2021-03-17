00d8eecb98bfcc4650af6010721bf41744ecd938
Istvan Percsi <istvanpercsi@gmail.com>
2021-03-17 20:42:20 +0100

    Initial app
    initial app

---
+++ we-applicaton/src/main/java/de/percsi/webEngine/WebEngineApplication.java
@@ -0,0 +1,13 @@
+package de.percsi.webEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEngineApplication.class, args);
+	}
+
+}
---
+++ we-applicaton/src/test/java/de/percsi/webEngine/WebEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package de.percsi.webEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
